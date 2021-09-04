7da0c86eb83196ebdbc9f63cc350b82618ef76e8
kmi0817 <kmi0817@naver.com>
2021-09-05 01:44:01 +0900

    researcherd에서 스미카 종류 보여주는 것까지 완료

--- app/static/irbAjax.js
+++ app/static/irbAjax.js
@@ -7,8 +7,29 @@ $(document).ready(() => {
         .done((res) => {
             var invitation = JSON.stringify(res.invitation); // 초대장 받기
             sessionStorage.setItem("invitation", invitation); // 초대장 세션 등록
-        })
-        .always(() => {
-            sessionStorage.setItem("temp", "[{'invitation': 'hi'}, {'user' : 'kmi'}]");
-        })
+
+            $.ajax({
+                type: "GET",
+                url: "http://localhost:8021/schemas/created",
+            })
+                .done((res) => {
+                    var schema_ids = res["schema_ids"];
+                    schema_ids.push("testSChemas");
+                    console.log(schema_ids);
+                    sessionStorage.setItem("schema_ids", schema_ids)
+                });
+        });
+
+    $("#logout").click(() => {
+        if (sessionStorage.getItem("connection_id")) {
+            var connection_id = sessionStorage.getItem("connection_id");
+            $.ajax({
+                type: "DELETE",
+                url: `http://localhost:8021/connections/${connection_id}`
+            })
+                .done((res) => {
+                    console.log("connection deleted")
+                });
+        }
+    })
 })
--- app/static/researcherAjax.js
+++ app/static/researcherAjax.js
@@ -1,8 +1,8 @@
 $(document).ready(() => {
     // 페이지 로드 시 세션에 있는 초대장 값 가져와 화면에 출력
-    sessionStorage.getItem("invitation");
-    var invitation = sessionStorage.getItem("temp");
-    $(".showInfo").html(invitation);
+    var invitation = sessionStorage.getItem("invitation");
+    // var invitation = sessionStorage.getItem("temp");
+    $(".showInfo").text(invitation);
 
     $("#acceptInvitation").click(() => {
         $.ajax({
@@ -11,17 +11,32 @@ $(document).ready(() => {
             data: invitation,
             datatype: "json"
         })
-            .always((res) => {
+            .done((res) => {
+                // console.log(res);
+                var schema_ids = sessionStorage.getItem("schema_ids");
+                schema_ids = schema_ids.split(",");
+                var schema_ids_tag = "<form>";
+                schema_ids.forEach((id) => {
+                    schema_ids_tag += `
+                        <label>
+                            <input type="radio" name="schema_ids">${id}</input><br>
+                        </label>
+                        `;
+                })
+                schema_ids_tag += "</form>";
+
+                console.log(schema_ids_tag);
                 var appended = `
                 <div class="div">
                     <div class="label">DID</div>
-                    <pre class="showInfo">did.. sample</pre>
+                    <div class="showInfo">${res["my_did"]}</div>
                 </div>
                 <div class="div">
-                    <div class="label">Credentials</div>
+                    <div class="label">schemas</div>
+                    <div class="showInfo">${schema_ids_tag}</div>
                 </div>
                 `;
                 $("#container").append(appended);
-            })
+            });
     })
 })
