9183f519d1677f887c24a78a80abf0b391d2aaff
kmi0817 <kmi0817@naver.com>
2021-09-06 09:49:42 +0900

    researcher : schema displaying completed

--- app/static/irbAjax.js
+++ app/static/irbAjax.js
@@ -13,14 +13,24 @@ $(document).ready(() => {
                 url: "http://localhost:8021/schemas/created",
             })
                 .done((res) => {
-                    var schema_ids = res["schema_ids"];
-                    schema_ids.push("testSChemas");
-                    console.log(schema_ids);
-                    sessionStorage.setItem("schema_ids", schema_ids)
+                    var schema_ids = res["schema_ids"]; // 스키마ID 배열로 반환
+                    sessionStorage.setItem("schema_ids", schema_ids); // 스키마ID 세션 등록
+
+                    // 반환받은 모든 스키마ID의 스키마를 세선에 등록함
+                    schema_ids.forEach((id) => {
+                        $.ajax({
+                            type: "GET",
+                            url: `http://localhost:8021/schemas/${id}`
+                        })
+                            .done((res) => {
+                                // 스키마ID에 해당하는 스키마 세션 등록
+                                sessionStorage.setItem(id, JSON.stringify(res));
+                            })
+                    });
                 });
         });
 
-    $("#logout").click(() => {
+    $("#logout").click(() => { // 이거 안 먹힘..
         if (sessionStorage.getItem("connection_id")) {
             var connection_id = sessionStorage.getItem("connection_id");
             $.ajax({
@@ -28,7 +38,8 @@ $(document).ready(() => {
                 url: `http://localhost:8021/connections/${connection_id}`
             })
                 .done((res) => {
-                    console.log("connection deleted")
+                    alert("connection deleted");
+                    sessionStorage.clear();
                 });
         }
     })
--- app/static/researcherAjax.js
+++ app/static/researcherAjax.js
@@ -12,31 +12,51 @@ $(document).ready(() => {
             datatype: "json"
         })
             .done((res) => {
-                // console.log(res);
-                var schema_ids = sessionStorage.getItem("schema_ids");
-                schema_ids = schema_ids.split(",");
-                var schema_ids_tag = "<form>";
+                var schema_ids = sessionStorage.getItem("schema_ids"); // 세션에서 스키마ID 가져오기
+                schema_ids = schema_ids.split(","); // 스키마ID 배열로 만들기 (string -> array)
+                var schema_ids_tag = ""; // 스키마ID 라디오 버튼 담을 태그
+                // 스키마ID 라디오 버튼 생성하여 태그에 추가
                 schema_ids.forEach((id) => {
                     schema_ids_tag += `
                         <label>
-                            <input type="radio" name="schema_ids">${id}</input><br>
+                            <input type="radio" name="schema_ids" value="${id}">${id}
                         </label>
+                        <br>
                         `;
                 })
-                schema_ids_tag += "</form>";
 
-                console.log(schema_ids_tag);
+                // 추가할 태그 1) DID 2) 스키마ID 라디오 버튼 3) 스키마
                 var appended = `
                 <div class="div">
                     <div class="label">DID</div>
                     <div class="showInfo">${res["my_did"]}</div>
                 </div>
                 <div class="div">
-                    <div class="label">schemas</div>
+                    <div class="label">schemaID</div>
                     <div class="showInfo">${schema_ids_tag}</div>
+                    <input type="button" id="selectSchemaId" class="btns" value="credential 선택">
+                </div>
+                <div class="div">
+                    <div class="label">스키마</div>
+                    <div id="displaySchema" class="showInfo"></div>
                 </div>
                 `;
                 $("#container").append(appended);
             });
-    })
+    });
+
+    $(document).on("click", "#selectSchemaId", () => {
+        var selectedId = $("input:radio[name=schema_ids]:checked").val();
+        if (selectedId) {
+            var schema = sessionStorage.getItem(selectedId);
+            console.log(schema);
+            if (schema) {
+                $("#displaySchema").html(schema);
+            } else {
+                alert("해당 스키마ID에 속하는 스키마가 없습니다.");
+            }
+        } else {
+            alert("스미카ID를 선택하세요.");
+        }
+    });
 })
